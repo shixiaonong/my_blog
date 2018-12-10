63f2cb12104aa607328d65038e21199fa089c054
shixiaonong <hegongda521>
2018-12-10 20:51:41 +0800

    支持markdown格式显示&导入css样式模板

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -21,5 +22,11 @@ def detail(request, pk):
     # 其作用就是当传入的 pk 对应的 Post 在数据库存在时，就返回对应的 post，
     # 如果不存在，就给用户返回一个 404 错误，表明用户请求的文章不存在。
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
 
